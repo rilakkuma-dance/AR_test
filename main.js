0e67fa8b76db337518d77ef73f9b6085b839f299
rilakkuma-dance <yuka.maruyama@student.uts.edu.au>
2025-01-31 04:15:19 +0000

    Add new screen

--- main.js
+++
@@ -1,8 +0,0 @@
-// main.js
-
-// Add an event listener for the "Go to Another Page" button
-document.getElementById("go-to-page-button").addEventListener("click", function() {
-    // Redirect to another page
-    window.location.href = "https://rilakkuma-dance.github.io/index.html";  // Change this to your desired page
-  });
-  
