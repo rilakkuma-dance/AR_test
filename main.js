460f6f2a15fbdaed910c036af7b243820e4c4f1b
rilakkuma-dance <yuka.maruyama@student.uts.edu.au>
2025-01-31 04:06:05 +0000

    Add new screen

--- main.js
+++ main.js
@@ -3,6 +3,6 @@
 // Add an event listener for the "Go to Another Page" button
 document.getElementById("go-to-page-button").addEventListener("click", function() {
     // Redirect to another page
-    window.location.href = "index.html";  // Change this to your desired page
+    window.location.href = "/workspaces/AR_test/index.html";  // Change this to your desired page
   });
   
