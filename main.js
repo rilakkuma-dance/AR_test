cfd25378e4881b5a3891eb2469f89164088f6c50
rilakkuma-dance <yuka.maruyama@student.uts.edu.au>
2025-01-31 03:51:09 +0000

    Add new screen

---
+++ main.js
@@ -0,0 +1,8 @@
+// main.js
+
+// Add an event listener for the "Go to Another Page" button
+document.getElementById("go-to-page-button").addEventListener("click", function() {
+    // Redirect to another page
+    window.location.href = "index.html";  // Change this to your desired page
+  });
+  
